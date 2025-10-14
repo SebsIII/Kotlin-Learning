68323fc9ba5b4f324064e087bcd28b75fcaf145a
SebsIII <cutillosebastiano@gmail.com>
2025-10-14 20:15:07 +0200

    Learning new things

--- First/src/main/kotlin/Main.kt
+++ First/src/main/kotlin/Main.kt
@@ -1,3 +1,10 @@
 fun main() {
+    println("Send an input and i will divide it by two!")
+    var input = readln().toIntOrNull() ?: 0
+    if(input != null) {
+        println("The output is ${input / 2}")
+    } else {
+        println("Your input wasn't valid.")
+    }
 
 }
